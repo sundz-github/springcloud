a5ca712b2106831b03ef1d2c52b0fd99c0647543
孙登志 <sundz@paraview.cn>
2020-12-17 13:36:09 +0800

    T# SpringCloud环境搭建

---
+++ springcloud-consumer/src/main/java/com/sundz/config/RestTemplateConfig.java
@@ -0,0 +1,31 @@
+package com.sundz.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * <p> 请求配置类——负载均衡 </p>
+ *
+ * @author Sundz
+ * @date 2020/12/17 13:33
+ */
+@Configuration
+public class RestTemplateConfig {
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    /**
+     * @field 客户端端负载均衡
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return restTemplateBuilder.build();
+    }
+
+}
--- springcloud-goods/src/main/java/com/sundz/SpringcloudApplicationGoods.java
+++ springcloud-goods/src/main/java/com/sundz/SpringcloudApplicationGoods.java
@@ -3,9 +3,11 @@ package com.sundz;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @Log4j2
+@EnableDiscoveryClient
 public class SpringcloudApplicationGoods {
 
     public static void main(String[] args) {
